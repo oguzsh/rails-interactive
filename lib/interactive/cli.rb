f6780940e4de9b26c95b55612b8d202c09a921ae
Oguzhan İnce <oguzhan824@gmail.com>
2022-03-12 02:40:25 +0300

    rubocop lint fix (#2)

--- lib/interactive/cli.rb
+++ lib/interactive/cli.rb
@@ -3,6 +3,7 @@
 require "tty-prompt"
 
 module Interactive
+  # CLI class for the interactive CLI module
   class CLI
     def initialize
       @prompt = TTY::Prompt.new
@@ -37,14 +38,16 @@ module Interactive
     end
 
     def render_ascii
+      # rubocop:disable Naming/HeredocDelimiterNaming
       puts <<-'EOF'
-  ___       _                      _   _           ____       _ _     
- |_ _|_ __ | |_ ___ _ __ __ _  ___| |_(_)_   _____|  _ \ __ _(_) |___ 
+  ___       _                      _   _           ____       _ _
+ |_ _|_ __ | |_ ___ _ __ __ _  ___| |_(_)_   _____|  _ \ __ _(_) |___
   | || '_ \| __/ _ \ '__/ _` |/ __| __| \ \ / / _ \ |_) / _` | | / __|
   | || | | | ||  __/ | | (_| | (__| |_| |\ V /  __/  _ < (_| | | \__ \
  |___|_| |_|\__\___|_|  \__,_|\___|\__|_| \_/ \___|_| \_\__,_|_|_|___/
 
       EOF
+      # rubocop:enable Naming/HeredocDelimiterNaming
     end
 
     def menu
