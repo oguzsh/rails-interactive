f453dbeeaf39de9ad2fd601d45db611a0ef723d0
Oguzhan Ince <oguzhan824@gmail.com>
2022-03-13 01:24:42 +0300

    init - prompt class

---
+++ lib/interactive/prompt.rb
@@ -0,0 +1,36 @@
+require "tty/prompt"
+
+module Interactive
+  # Prompt class for commands
+  class Prompt
+    # Create a new instance
+    #
+    # @param msg [String] the message to display
+    # @param type [String] the type of prompt
+    # @param options [Array] the options to display
+    # @param required [Boolean] whether the prompt value is required
+    #
+    # @return [Interactive::Prompt] the new instance
+    def initialize(msg, type, options = nil, required: false)
+      @msg = msg
+      @type = type
+      @options = options
+      @required = required
+      @prompt = TTY::Prompt.new
+    end
+
+    # Perform the prompt
+    #
+    # @return [String] the value of the prompt
+    def perform
+      case @type
+      when "ask"
+        @prompt.ask(@msg, required: @required)
+      when "select"
+        @prompt.select(@msg, @options, required: @required)
+      else
+        puts "Invalid parameter"
+      end
+    end
+  end
+end
