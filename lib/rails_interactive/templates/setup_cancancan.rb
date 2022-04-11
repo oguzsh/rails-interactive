1e54e9136e568aa56deaeb27e6c08ad3b587baf6
Oguzhan Ince <oguzhan824@gmail.com>
2022-04-12 00:37:54 +0300

    rubocop - fix

--- lib/rails_interactive/templates/setup_cancancan.rb
+++ lib/rails_interactive/templates/setup_cancancan.rb
@@ -1,5 +1,7 @@
-run 'bundle add cancancan'
+# frozen_string_literal: true
+
+run "bundle add cancancan"
 
 Bundler.with_unbundled_env { run "bundle install" }
 
-rails_command 'generate cancan:ability'
+rails_command "generate cancan:ability"
