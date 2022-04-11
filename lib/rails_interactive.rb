12fd27a061179f5d14be729bdd7c3428ddff0d39
Oguzhan Ince <oguzhan824@gmail.com>
2022-04-11 04:34:33 +0300

    multi select option - init

--- lib/rails_interactive.rb
+++ lib/rails_interactive.rb
@@ -2,6 +2,7 @@
 
 require "rails_interactive/prompt"
 require "rails_interactive/message"
+require "fileutils"
 
 module RailsInteractive
   # CLI class for the interactive CLI module
@@ -31,6 +32,9 @@ module RailsInteractive
       database_types = { "PostgreSQL" => "-d postgresql", "MySQL" => "-d mysql", "SQLite" => "" }
       @inputs[:database] = Prompt.new("Choose project's database: ", "select", database_types, required: true).perform
 
+      features = %w[devise]
+      @inputs[:features] = Prompt.new("Choose project features: ", "multi_select", features).perform
+
       create
     end
 
@@ -39,6 +43,15 @@ module RailsInteractive
       system("bin/setup")
       # Create project
       system(setup)
+
+      copy_templates_to_project
+
+      # Move to project folder and install gems
+      Dir.chdir "./#{@inputs[:name]}"
+      @inputs[:features].each do |feature|
+        system("bin/rails app:template LOCATION=templates/setup_#{feature}.rb")
+      end
+
       # Prepare project requirements and give instructions
       Message.prepare
     end
@@ -50,6 +63,10 @@ module RailsInteractive
       @inputs.each { |_key, value| cmd += "#{value} " }
 
       "#{base} #{cmd}".strip!
+    end
+
+    def copy_templates_to_project
+      FileUtils.cp_r "#{File.expand_path(File.dirname(__FILE__))}/rails_interactive/templates", "./#{@inputs[:name]}"
     end
   end
 end
