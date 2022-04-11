52767d7ff4aeee3d0a752e4ec0112d9989e8fa26
Oguzhan Ince <oguzhan824@gmail.com>
2022-04-12 01:57:30 +0300

    rubocop - fix

--- lib/rails_interactive/templates/setup_sidekiq.rb
+++ lib/rails_interactive/templates/setup_sidekiq.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
+
 def file_contains?(filename, string)
   File.foreach(filename).detect { |line| line.include?(string) }
 end
 
 run "bundle add sidekiq"
-run "bundle add redis" unless file_contains? 'Gemfile', "Gem 'redis'"
+run "bundle add redis" unless file_contains? "Gemfile", "Gem 'redis'"
 
-Bundler.with_unbundled_env { run 'bundle install' }
+Bundler.with_unbundled_env { run "bundle install" }
 
-application do <<~EOF
-  config.active_job.queue_adapter = :sidekiq
+application do
+  <<~EOF
+    config.active_job.queue_adapter = :sidekiq
   EOF
 end
 
-inject_into_file 'config/routes.rb' do <<~EOF
-  require "sidekiq/web"
-  if Rails.env.production?
-    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
-        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+inject_into_file "config/routes.rb" do
+  <<~EOF
+    require "sidekiq/web"
+    if Rails.env.production?
+      Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+          ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+      end
     end
-  end
   EOF
 end
 
