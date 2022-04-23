cce9cb2756c699f59cfc789eae8b68aeee3462bf
Oguzhan Ince <oguzhan824@gmail.com>
2022-04-24 00:14:23 +0300

    rubocop fix

--- lib/rails_interactive/templates/setup_rubocop.rb
+++ lib/rails_interactive/templates/setup_rubocop.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 gem_group :development do
-  gem 'rubocop', require: false
+  gem "rubocop", require: false
 end
 
-Bundler.with_unbundled_env { run 'bundle install' }
+Bundler.with_unbundled_env { run "bundle install" }
 
 run "rubocop --auto-gen-config"
 run "bundle binstubs rubocop"
