8c6b4a2bf395a2e3fe585f5ef614b8179dbd5857
Oguzhan Ince <oguzhan824@gmail.com>
2022-04-24 01:03:25 +0300

    fix db command

--- lib/rails_interactive/templates/setup_active_admin.rb
+++ lib/rails_interactive/templates/setup_active_admin.rb
@@ -3,19 +3,12 @@ run "bundle add bcrypt"
 Bundler.with_unbundled_env { run "bundle" }
 
 if yes?("Do you want to install Devise?(prefered)[y/n]") && !defined?(Devise)
-  run "rails db:create"
+  run "rails db:prepare"
   run "bundle add devise"
   rails_command "generate active_admin:install"
 else
+  run "rails db:prepare"
   model = ask("What is the user model you want to use for Active Admin? (ex: User)")
   rails_command "generate active_admin:install #{model.capitalize}" if model
 end
-rails_command "generate active_admin:webpacker"
 rails_command "db:migrate"
-
-while yes?("Do you want to add an other model to ActiveAdmin?[y/n]") do
-  model = ask('Model name (ex: Post or MyPost):')
-  if model
-    rails_command "g active_admin:resource #{model}"
-  end
-end
