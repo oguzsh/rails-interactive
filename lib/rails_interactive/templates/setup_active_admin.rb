da09018270254fba6bc616bdaf32b1bfac9523b9
Oguzhan Ince <oguzhan824@gmail.com>
2022-04-24 01:41:40 +0300

    methods fix

--- lib/rails_interactive/templates/setup_active_admin.rb
+++ lib/rails_interactive/templates/setup_active_admin.rb
@@ -1,16 +1,4 @@
 # frozen_string_literal: true
-run "bundle add activeadmin"
-run "bundle add bcrypt"
-Bundler.with_unbundled_env { run "bundle" }
-
-if !file_contains?("Gemfile", "Gem 'devise'")
-  active_admin_install_with_devise
-else
-  active_admin_install_without_devise
-end
-rails_command "db:migrate"
-
-
 def file_contains?(filename, string)
   File.foreach(filename).detect { |line| line.include?(string) }
 end
@@ -24,3 +12,14 @@ def active_admin_install_with_devise
   system("bin/rails app:template LOCATION=templates/setup_devise.rb")
   rails_command "generate active_admin:install"
 end
+
+run "bundle add activeadmin"
+run "bundle add bcrypt"
+Bundler.with_unbundled_env { run "bundle" }
+
+if !file_contains?("Gemfile", "Gem 'devise'")
+  active_admin_install_with_devise
+else
+  active_admin_install_without_devise
+end
+rails_command "db:migrate"
