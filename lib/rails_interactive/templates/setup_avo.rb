76cbc70d50a9e4a6aa79cdca0172a345c4fe2309
Oguzhan Ince <oguzhan824@gmail.com>
2022-04-24 02:57:22 +0300

    edit: message

--- lib/rails_interactive/templates/setup_avo.rb
+++ lib/rails_interactive/templates/setup_avo.rb
@@ -7,4 +7,4 @@ Bundler.with_unbundled_env { run "bundle install" }
 
 rails_command("generate avo:install")
 
-puts "Avo is installed! You can go to https://docs.avohq.io/2.0/resources.html for next steps"
+puts "Avo is installed! You can go to http://localhost:3000/avo for next steps"
