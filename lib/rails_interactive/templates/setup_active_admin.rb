89b1e7adc42f51174557be7c8d7f79922451e4a4
Oguzhan Ince <oguzhan824@gmail.com>
2022-04-24 01:39:33 +0300

    simplified

--- lib/rails_interactive/templates/setup_active_admin.rb
+++ lib/rails_interactive/templates/setup_active_admin.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
+run "bundle add activeadmin"
+run "bundle add bcrypt"
+Bundler.with_unbundled_env { run "bundle" }
+
+if !file_contains?("Gemfile", "Gem 'devise'")
+  active_admin_install_with_devise
+else
+  active_admin_install_without_devise
+end
+rails_command "db:migrate"
+
 
 def file_contains?(filename, string)
   File.foreach(filename).detect { |line| line.include?(string) }
 end
 
-run "bundle add activeadmin"
-run "bundle add bcrypt"
-Bundler.with_unbundled_env { run "bundle" }
-
-run "rails db:prepare"
-if !file_contains?("Gemfile", "Gem 'devise'") && yes?("Do you want to install Devise?(prefered)[y/n]")
-    run "bundle add devise"
-    rails_command "generate active_admin:install"
-else
-  model = ask("What is the user model you want to use for Active Admin? (ex: User)")
+def active_admin_install_without_devise
+  model = ask("What is the user model you want to use for Active Admin? (ex: Admin)")
   rails_command "generate active_admin:install #{model.capitalize}" if model
 end
-rails_command "db:migrate"
+
+def active_admin_install_with_devise
+  system("bin/rails app:template LOCATION=templates/setup_devise.rb")
+  rails_command "generate active_admin:install"
+end
