937ed20e7fdd1c2e6bf74fa3a6742e4ec0303c32
Oguzhan Ince <oguzhan824@gmail.com>
2022-04-24 00:43:13 +0300

    init: active_admin

---
+++ lib/rails_interactive/templates/setup_active_admin.rb
@@ -0,0 +1,19 @@
+run "bundle add activeadmin"
+run "bundle add bcrypt"
+
+if yes?("Do you want to install Devise?(prefered)[y/n]") && !defined?(Devise)
+  run "bundle add devise"
+  rails_command "generate active_admin:install"
+else
+  model = ask("What is the user model you want to use for Active Admin? (ex: User)")
+  rails_command "generate active_admin:install #{model.capitalize}" if model
+end
+rails_command "generate active_admin:webpacker"
+rails_command "db:migrate"
+
+while yes?("Do you want to add an other model to ActiveAdmin?[y/n]") do
+  model = ask('Model name (ex: Post or MyPost):')
+  if model
+    rails_command "g active_admin:resource #{model}"
+  end
+end
