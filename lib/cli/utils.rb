ca2fad05983d6d1b3f9f66c1629e15e07645b94c
Oguzhan Ince <oguzhan824@gmail.com>
2022-05-30 02:27:59 +0300

    update: typo

--- lib/cli/utils.rb
+++ lib/cli/utils.rb
@@ -14,27 +14,27 @@ module RailsInteractive
           .gsub(/^[a-z]/, &:upcase)
       end
 
-      def self.go_to_project_directory(project_name)
-        FileUtils.rm_rf("./#{project_name}/templates")
+      def self.remove_templates(_project_name)
+        FileUtils.rm_rf("templates")
       end
 
-      def self.remove_templates(project_name)
+      def self.go_to_project_directory(project_name)
         Dir.chdir "./#{project_name}"
       end
 
       def self.copy_templates_to_project(project_name)
-        FileUtils.cp_r "#{__dir__}/rails_interactive/templates", "./#{project_name}"
+        FileUtils.cp_r "#{__dir__}/templates", "./#{project_name}"
 
         go_to_project_directory(project_name)
       end
 
-      def handle_multi_options(multi_options)
+      def self.handle_multi_options(multi_options)
         multi_options.each do |value|
           system("bin/rails app:template LOCATION=templates/setup_#{value}.rb")
         end
       end
 
-      def handle_option(option)
+      def self.handle_option(option)
         system("bin/rails app:template LOCATION=templates/setup_#{option}.rb")
       end
 
