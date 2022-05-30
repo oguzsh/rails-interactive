8fc10922bfa9693d4d7cb3be86bdef7e59394720
Oguzhan Ince <oguzhan824@gmail.com>
2022-05-31 01:09:06 +0300

    fix: null check and parameters activated

--- lib/cli/command_handler.rb
+++ lib/cli/command_handler.rb
@@ -11,18 +11,19 @@ module RailsInteractive
         @commands = Command.new.all
       end
 
-      def handle_multi_options_(options, dependencies = nil)
-        handle_dependencies(dependencies) if dependencies
+      def handle_multi_options(options, dependencies = nil)
+        handle_dependencies(dependencies)
         options.each { |option| system("bin/rails app:template LOCATION=templates/setup_#{option}.rb") }
       end
 
       def handle_option(option, dependencies = nil)
-        handle_dependencies(dependencies) if dependencies
+        handle_dependencies(dependencies)
         system("bin/rails app:template LOCATION=templates/setup_#{option}.rb")
       end
 
-      def handle_dependencies(_options)
-        dependencies.each do |dependency|
+      def handle_dependencies(dependencies)
+        dependencies&.each do |dependency|
+          puts ">> Dependency Detected: #{dependency} "
           system("bin/rails app:template LOCATION=templates/setup_#{dependency}.rb")
         end
       end
