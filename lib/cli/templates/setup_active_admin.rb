de02bb4e126085effafdd691673d8fa02a71e003
Oguzhan Ince <oguzhan824@gmail.com>
2022-06-01 01:17:20 +0300

    fix: active admin template

--- lib/cli/templates/setup_active_admin.rb
+++ lib/cli/templates/setup_active_admin.rb
@@ -1,21 +1,8 @@
 # frozen_string_literal: true
 
-def active_admin_install_without_devise
-  model = ask("What is the user model you want to use for Active Admin? (ex: Admin)")
-  rails_command "generate active_admin:install #{model.capitalize}" if model
-end
-
-def active_admin_install_with_devise
-  rails_command "generate active_admin:install"
-end
-
 run "bundle add activeadmin"
 run "bundle add bcrypt"
 Bundler.with_unbundled_env { run "bundle" }
 
-if !defined?(Devise)
-  active_admin_install_without_devise
-else
-  active_admin_install_with_devise
-end
+rails_command "generate active_admin:install"
 rails_command "db:migrate"
