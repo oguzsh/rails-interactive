5c9b6e2d42ee244398e423314568f4b135d3ca45
Oguzhan Ince <oguzhan824@gmail.com>
2022-06-05 04:51:57 +0300

    add: empty check

--- lib/cli/templates/setup_devise.rb
+++ lib/cli/templates/setup_devise.rb
@@ -6,6 +6,8 @@ Bundler.with_unbundled_env { run "bundle install" }
 rails_command "generate devise:install"
 
 model_name = ask("What do you want to call your Devise model?")
+model_name = model_name.empty? ? "user" : model_name
+
 File.open("devise-model.txt", "w") {|f| f.write(model_name) }
 
 run "rails generate devise #{model_name.capitalize}"
