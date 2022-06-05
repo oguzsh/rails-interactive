32cd4513b7d9c0898dbe62bdf0a605074b4ba226
Oguzhan Ince <oguzhan824@gmail.com>
2022-06-05 04:46:32 +0300

    add: dynamic authentication model name

--- lib/cli/templates/setup_devise.rb
+++ lib/cli/templates/setup_devise.rb
@@ -5,6 +5,9 @@ Bundler.with_unbundled_env { run "bundle install" }
 
 rails_command "generate devise:install"
 
-run "rails generate devise User"
+model_name = ask("What do you want to call your Devise model?")
+File.open("devise-model.txt", "w") {|f| f.write(model_name) }
+
+run "rails generate devise #{model_name.capitalize}"
 run "rails db:prepare"
 run "rails db:migrate"
--- lib/cli/templates/setup_omniauth.rb
+++ lib/cli/templates/setup_omniauth.rb
@@ -1,30 +1,34 @@
 # frozen_string_literal: true
+require "fileutils"
 
 run "bundle add omniauth"
 
 run "bundle install"
 
+devise_model_name = File.read("devise-model.txt")
+FileUtils.rm("devise-model.txt")
+
 # rubocop:disable Layout/LineLength
-rails_command "generate model identity user:references provider:string:index uid:string:index token:string:index refresh_token:string:index"
+rails_command "generate model identity #{devise_model_name}:references provider:string:index uid:string:index token:string:index refresh_token:string:index"
 # rubocop:enable Layout/LineLength
 
-rails_command "generate migration AddIdentityToUsers identities_count:integer"
+rails_command "generate migration AddIdentityTo#{devise_model_name.capitalize}s identities_count:integer"
 
 rails_command "db:migrate"
 
-inject_into_file "config/routes.rb", after: "devise_for :users\n" do
-  " # devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }"
+inject_into_file "config/routes.rb", after: "devise_for :#{devise_model_name}s\n" do
+  " # devise_for :#{devise_model_name}s, controllers: { omniauth_callbacks: 'omniauth' }"
 end
 
-inject_into_file "app/models/user.rb", after: ":database_authenticatable, " do
+inject_into_file "app/models/#{devise_model_name}.rb", after: ":database_authenticatable, " do
   ":omniauthable, "
 end
 
-inject_into_file "app/models/identity.rb", after: "belongs_to :user" do
+inject_into_file "app/models/identity.rb", after: "belongs_to :#{devise_model_name}" do
   ", counter_cache: true"
 end
 
-inject_into_file "app/models/user.rb", after: "class User < ApplicationRecord\n" do
+inject_into_file "app/models/#{devise_model_name}.rb", after: "class #{devise_model_name.capitalize} < ApplicationRecord\n" do
   # rubocop:disable Naming/HeredocDelimiterNaming
   <<-EOF
   has_many :identities, dependent: :destroy
@@ -36,15 +40,15 @@ inject_into_file "app/models/user.rb", after: "class User < ApplicationRecord\n"
         identity.token = auth.credentials.token
         identity.refresh_token = auth.credentials.refresh_token
       end
-      if identity.user.nil? && auth.info.email.present?
-        user = User.where(email: auth.info.email).first_or_initialize
+      if identity.#{devise_model_name}.nil? && auth.info.email.present?
+        user = #{devise_model_name}.where(email: auth.info.email).first_or_initialize
         user.name = auth.info.name
         user.password = Devise.friendly_token if user.new_record?
         user.save!
-        identity.user = user
+        identity.#{devise_model_name} = user
       end
       identity.save!
-      identity.user
+      identity.#{devise_model_name}
     end
   end
 
@@ -58,4 +62,4 @@ file "app/controllers/omniauth_controller.rb", <<~CODE
   end
 CODE
 
-puts "IMPORTANT: Add devise_for :users, controllers: { omniauth_callbacks: 'omniauth' } to your routes.rb"
+puts "IMPORTANT: Add devise_for :#{devise_model_name}s, controllers: { omniauth_callbacks: 'omniauth' } to your routes.rb"
